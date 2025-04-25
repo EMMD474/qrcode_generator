73eefed9c560857d34b0d7218f01581eb1dfde17
EMMD474 <96117380+EMMD474@users.noreply.github.com>
2025-04-25 15:49:41 +0200

    modified the tsconfig file

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,42 @@
+"use client"
+
 import QRForm from './components/QRForm';
 import { Toaster } from 'sonner';
+import Head from "next/head"
+import { useEffect } from 'react';
 
 export default function Home() {
-  
+
+  useEffect(() => {
+    document.title = "QR Code Ready!";
+  }, []);
+
+
  
   return (
-    <div 
-      className="h-screen w-screen bg-cover bg-center flex flex-col items-center justify-center" 
-      style={{ backgroundImage: "url('/background.png')" }}
-    >
-      <Toaster position='top-right' className='bg-gray-800' />
-      <div 
-        className='bg-gray-900 rounded-lg shadow-lg p-6' 
-      >
-        <h2 className='flex flex-row items-center text-gray-700 text-2xl font-bold'>
-          <span className='text-white mr-2'>QR-Code </span>
-          Generator
-        </h2>
-        <QRForm />
-      </div>
-    </div>
+    <>
+    <Head>
+      <title>
+        QR Code Generator
+      </title>
+    </Head>
+      <main>
+        <div 
+          className="h-screen w-screen bg-cover bg-center flex flex-col items-center justify-center" 
+          style={{ backgroundImage: "url('/background.png')" }}
+        >
+          <Toaster position='top-right' className='bg-gray-800' />
+          <div 
+            className='bg-gray-900 rounded-lg shadow-lg p-6' 
+          >
+            <h2 className='flex flex-row items-center text-gray-700 text-2xl font-bold'>
+              <span className='text-white mr-2'>QR-Code </span>
+              Generator
+            </h2>
+            <QRForm />
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
